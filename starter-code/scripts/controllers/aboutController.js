e193a9c2ed0949f8d71adb82763e754492013a09
Julien Wilson <julienawilson@gmail.com>
2016-11-07 15:54:08 -0800

    added the aboutController.reveal method

--- starter-code/scripts/controllers/aboutController.js
+++ starter-code/scripts/controllers/aboutController.js
@@ -3,6 +3,8 @@
 
   aboutController.reveal = function() {
     /* TODO: Use your DOM skills to reveal only the about section! */
+    $('main section').hide();
+    $('#articles').show();
   };
 
   module.aboutController = aboutController;
