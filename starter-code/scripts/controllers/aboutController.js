c9bccbb4522bd8283a4add25230827ba75bc8a22
reganoneill <rkrotho@gmail.com>
2016-11-07 16:26:03 -0800

    completed all TODOs

--- starter-code/scripts/controllers/aboutController.js
+++ starter-code/scripts/controllers/aboutController.js
@@ -3,7 +3,8 @@
 
   aboutController.reveal = function() {
     /* TODO: Use your DOM skills to reveal only the about section! */
-
+    $('#articles').hide();
+    $('#about').show();
   };
 
   module.aboutController = aboutController;
