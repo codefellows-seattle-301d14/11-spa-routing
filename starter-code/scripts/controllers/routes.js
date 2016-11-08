c067e76fa30cfc4bfe02b0eb3ed3c6ad9f778082
Julien Wilson <julienawilson@gmail.com>
2016-11-07 16:00:09 -0800

    included routes.js functons to control the page

--- starter-code/scripts/controllers/routes.js
+++ starter-code/scripts/controllers/routes.js
@@ -1,5 +1,9 @@
-/* TODO: Configure our two routes for this app with page.js,
+/* TODO:DONE Configure our two routes for this app with page.js,
     by registering each URL your app can handle, linked to a
     single controller function to handle it: */
+page();
+page('/', articleController.reveal);
+page('/about',aboutController.reveal);
 
-// TODO: What function do we call to activate page.js?
+
+// TODO: DONE (See above) What function do we call to activate page.js?
