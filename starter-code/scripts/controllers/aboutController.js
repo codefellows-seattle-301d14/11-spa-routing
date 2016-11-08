d6760c7ed953728cfe91e5dd0e6c64ab5abf4d12
Chaimaebettah <bettah.chaimae@gmail.com>
2016-11-07 16:57:08 -0800

    soekgjg

--- starter-code/scripts/controllers/aboutController.js
+++ starter-code/scripts/controllers/aboutController.js
@@ -4,6 +4,7 @@
   aboutController.reveal = function() {
     /* TODO: Use your DOM skills to reveal only the about section! */
     $('main').hide().not('#about');
+    
   };
 
   module.aboutController = aboutController;
